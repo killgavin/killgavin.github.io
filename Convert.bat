8b9f90a2e0ef6d43a51ad239e9851ccba8006ab9
Gavin Liang <aple0620@gmail.com>
2022-03-14 15:00:16 +0800

    first

---
+++ Convert.bat
@@ -0,0 +1,44 @@
+ffmpeg -i 001.mp3 001.ogg
+ffmpeg -i 001.mp3 001.m4a
+ffmpeg -i 001.mp3 001.wav
+ffmpeg -i 001.mp3 001.flac
+
+ffmpeg -i 002.mp3 002.ogg
+ffmpeg -i 002.mp3 002.m4a
+ffmpeg -i 002.mp3 002.wav
+ffmpeg -i 002.mp3 002.flac
+
+ffmpeg -i 003.mp3 003.ogg
+ffmpeg -i 003.mp3 003.m4a
+ffmpeg -i 003.mp3 003.wav
+ffmpeg -i 003.mp3 003.flac
+
+ffmpeg -i 004.mp3 004.ogg
+ffmpeg -i 004.mp3 004.m4a
+ffmpeg -i 004.mp3 004.wav
+ffmpeg -i 004.mp3 004.flac
+
+ffmpeg -i 005.mp3 005.ogg
+ffmpeg -i 005.mp3 005.m4a
+ffmpeg -i 005.mp3 005.wav
+ffmpeg -i 005.mp3 005.flac
+
+ffmpeg -i 006.mp3 006.ogg
+ffmpeg -i 006.mp3 006.m4a
+ffmpeg -i 006.mp3 006.wav
+ffmpeg -i 006.mp3 006.flac
+
+ffmpeg -i 007.mp3 007.ogg
+ffmpeg -i 007.mp3 007.m4a
+ffmpeg -i 007.mp3 007.wav
+ffmpeg -i 007.mp3 007.flac
+
+ffmpeg -i 008.mp3 008.ogg
+ffmpeg -i 008.mp3 008.m4a
+ffmpeg -i 008.mp3 008.wav
+ffmpeg -i 008.mp3 008.flac
+
+ffmpeg -i 009.mp3 009.ogg
+ffmpeg -i 009.mp3 009.m4a
+ffmpeg -i 009.mp3 009.wav
+ffmpeg -i 009.mp3 009.flac
